bd068be4ce42eaee2eda47fb2a85509e2969b9df
geekrelief <geekrelief@gmail.com>
2021-10-01 16:15:46 -0700

    updated to use just tm_dir

--- tm_gen.nim
+++ tm_gen.nim
@@ -1,8 +1,7 @@
 import nimterop/[cimport, paths]
 import os, strformat, sugar, strutils, sequtils, macros
 
-const tm_nim_dir = "C:/tm/tm-nim/" # this repo's path
-const tm_dir = "C:/tm/gr-tm"
+const tm_dir = "C:/tm/tm-nim"
 
 static:
   #cDebug()
@@ -10,7 +9,7 @@ static:
 
 proc getHeaders():seq[string] =
   result = collect:
-    for i in walkDir(&"{tm_dir}/foundation"):
+    for i in walkDir(&"{tm_dir}"):
       if i.kind == pcFile and i.path.endsWith(".h"):
         i.path
 
@@ -27,7 +26,5 @@ cOverride:
 #cImport(filenames = static(getHeaders()), nimFile = "tm.nim")
 cImport( flags = "-E_ -F_ -G__=_", recurse = true,
   nimFile = "tm.nim", 
-  filenames = @[
-    tm_nim_dir & "api_registry.h",
-    tm_nim_dir & "log.h",
-  ])
+  filenames = static(getHeaders())
+)
