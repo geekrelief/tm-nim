561faaee9f1bdf81cfc1557afbf8757c953c4f7a
geekrelief <geekrelief@gmail.com>
2021-10-13 01:15:17 -0700

    cleaned up custom_simulation_entry

--- samples/plugins/custom_simulation_entry.nim
+++ samples/plugins/custom_simulation_entry.nim
@@ -2,27 +2,24 @@
 include tm
 
 import strformat
-
-const version = TM_VERSION(0, 1, 0)
-
-var logger: ptr tm_logger_api
-var count = 0
+const 
+  version = TM_VERSION(0, 1, 0)
+var 
+  logger: ptr tm_logger_api
+  count = 0
 
 proc start(args: ptr tm_simulation_start_args_t): ptr tm_simulation_state_o {.cdecl.} =
-  let state = cast[ptr tm_simulation_state_o](tm_alloc(args[].allocator, sizeof(tm_simulation_state_o).uint64))
-  state[] = tm_simulation_state_o(allocator: args[].allocator)
+  result = tm_alloc(args[].allocator, tm_simulation_state_o(allocator: args[].allocator))
   logger.print(TM_LOG_TYPE_INFO, $<"start simulation")
-  state
 
 proc stop(state: ptr tm_simulation_state_o, commands: ptr tm_entity_commands_o) {.cdecl.} =
-  let a = state[].allocator
-  tm_free(a, state, sizeof(state[]).uint64)
+  tm_free(state[].allocator, state)
   logger.print(TM_LOG_TYPE_INFO, $<"stop simulation")
 
 proc tick(state: ptr tm_simulation_state_o, args: ptr tm_simulation_frame_args_t) {.cdecl.} =
   # called once a frame
   inc count
-  logger.print(TM_LOG_TYPE_INFO, $< &"tick {count=}!")
+  logger.print(TM_LOG_TYPE_INFO, $< &"!!tock {count=}!")
 
 var simulation_entry_i = tm_simulation_entry_i(
   id: TM_STATIC_HASH("tm_custom_simulation_entry"),
--- tm/foundation/allocator.nim
+++ tm/foundation/allocator.nim
@@ -1,16 +1,18 @@
-# Convenience macro for allocating memory using [[tm_allocator_i]].
-#define tm_alloc(a, sz) (a)->realloc(a, 0, 0, sz, __FILE__, __LINE__)
-template tm_alloc*(a: ptr tm_allocator_i, sz: uint64): untyped =
-  let pos = instantiationInfo()
-  a[].realloc(a, cast[pointer](0), 0'u64, sz, $< pos.filename, pos.line.uint32)
+macro tm_alloc*(a: ptr tm_allocator_i, init: untyped): untyped =
+  let TYPE = init[0]
+  result = quote do:
+    let pos = instantiationInfo()
+    let p = cast[ptr `TYPE`](`a`[].realloc(`a`, cast[pointer](0), 0, sizeof(`TYPE`).uint64, $< pos.filename, pos.line.uint32))
+    p[] = `init`
+    p
 
 #define tm_alloc_at(a, sz, file, line) (a)->realloc(a, 0, 0, sz, file, line)
 
 #Convenience macro for freeing memory using [[tm_allocator_i]].
 #define tm_free(a, p, sz) (a)->realloc(a, p, sz, 0, __FILE__, __LINE__)
-template tm_free*(a: ptr tm_allocator_i, p: pointer, sz: uint64) =
+template tm_free*(a: ptr tm_allocator_i, p: untyped) =
   let pos = instantiationInfo()
-  discard a[].realloc(a, p, sz, 0'u64, $< pos.filename, pos.line.uint32)
+  discard a[].realloc(a, p, sizeof(p[]).uint64, 0, $< pos.filename, pos.line.uint32)
 
 # Convenience macro for reallocating memory using [[tm_allocator_i]].
 #define tm_realloc(a, p, old_sz, new_sz) (a)->realloc(a, p, old_sz, new_sz, __FILE__, __LINE__)
