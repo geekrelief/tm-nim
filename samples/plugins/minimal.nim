bb499d67541dc90de6061ae4c0886cf591f8b039
geekrelief <geekrelief@gmail.com>
2021-10-13 22:18:06 -0700

    updated minimal

--- samples/plugins/minimal.nim
+++ samples/plugins/minimal.nim
@@ -9,9 +9,9 @@ proc tm_load_plugin(reg: ptr tm_api_registry_api, load: bool) {.callback.} =
 
   let logger = reg.tm_get_api(tm_logger_api)
 
-  logger.print(TM_LOG_TYPE_INFO, $<(&"{version=}: " & (if not load: "Unloading old plugin" else: "Hello from Nim!")))
+  logger.print(TM_LOG_TYPE_INFO, cstring(&"{version=}: " & (if not load: "Unloading old plugin" else: "Hello from Nim!")))
 
   if load:
     var id = tt_id(3, 2, 1)
-    logger.print(TM_LOG_TYPE_INFO, $< &"{id.`type`=} {id.generation=} {id.index=} {tm_tt_type_t(id).u64=}")
-    logger.print(TM_LOG_TYPE_INFO, $< &"{TM_STATIC_HASH(\"minimal\").uint64 = :#x}")
+    logger.print(TM_LOG_TYPE_INFO, cstring &"{id.`type`=} {id.generation=} {id.index=} {tm_tt_type_t(id).u64=}")
+    logger.print(TM_LOG_TYPE_INFO, cstring &"{TM_STATIC_HASH(\"minimal\").uint64 = :#x}")
